27655c0e99ba81403b12e998a32e6599e6cb3d06
Yash Pandey <ypandey777@yahoo.in>
2024-11-17 00:54:52 +0530

    Add cors, body parser and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,29 @@
 // backend/index.js
 const express = require("express");
 const rootRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+const cors = require("cors");
 
-const app = express();
+const app = express.Router();
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1", rootRouter);
+app.use("/api/v1/user",userRouter);
+
+// Example route to create a new user
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, password, firstName, lastName } = req.body;
+        const newUser = new User({ username, password, firstName, lastName });
+        await newUser.save();
+        res.status(201).send({ message: 'User created successfully' });
+    } catch (err) {
+        res.status(500).send({ error: 'Failed to create user' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,27 +3,13 @@ const express = require('express');
 const router = express.Router();
 const userRouter = require("./user");
 const User = require('./models/User');  // Import the User model
+const cors = require("cors");
 
+app.use(cors());
 const app = express.Router();
 
 router.use("/user", userRouter)
 
 app.use(express.json());
-
-// Example route to create a new user
-app.post('/signup', async (req, res) => {
-    try {
-        const { username, password, firstName, lastName } = req.body;
-        const newUser = new User({ username, password, firstName, lastName });
-        await newUser.save();
-        res.status(201).send({ message: 'User created successfully' });
-    } catch (err) {
-        res.status(500).send({ error: 'Failed to create user' });
-    }
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
 
 module.exports = router ;
